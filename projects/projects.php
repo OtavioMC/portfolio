bad7b02710bbe9b501447c2b18f0480a043eb7de
OtavioMC <mendoncacostaotavio@gmail.com>
2024-02-24 10:07:28 -0300

    ADD: Includes new project view and http error pages

---
+++ projects/projects.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <title>Otávio Costa - Portfolio</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../assets/index.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-xs-12">
+                <div class="text-center" style="padding-top: 30px; padding-bottom: 30px;">
+                    <h2 style="color: #ffffff; padding-top: 20px;">My Projects</h2>
+                </div>
+        </div>
+    </div>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-dark table-striped table-bordered mh-20">
+                                        <thead>
+                                            <tr>
+                                                <th> PROJECT </th>
+                                                <th> SOURCE CODE </th>
+                                                <th> WEB LINK </th>
+                                                <th> DESCRIPTION </th>
+                                                <th> BACKEND STACK </th>
+                                                <th> FRONTEND STACK </th>
+                                                <th> OTHER TECH </th>
+                                                <th> TAG </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td> Customerapp ( written in Brazilian Portuguese ) </td>
+                                                <td><a href='https://github.com/OtavioMC/customerapp' target="_blank"> GitHub </a></td>
+                                                <td> <a href='http://159.112.186.221' target="_blank"> <b>Oracle Cloud</b> </a> </td>
+                                                <td> The customerapp is a web application that allows you to keep your store's customer records up to date. </td>
+                                                <td> <b> PHP 8 </b> (pure) </td>
+                                                <td> <b>Html 5, CSS, Bootstrap 5, JQuery, Javascript </b> </td>
+                                                <td> <b> MySQL </b>, <br><b>Kahlan</b> - PHP Unit Tests , <br><b>Phpstan</b> - for Code Coverage ,<br> <b> Captainhook </b> - Githook Manager for PHP, <br><b>Phinx</b> - PHP DB Migration Manager <b>and more.</b> </td>
+                                                <td> <b>1.1.1</b> </td>
+                                            </tr>
+                                            <tr>
+                                                <td> Exptracker </td>
+                                                <td><a href='https://github.com/OtavioMC/exptracker' target="_blank"> GitHub </a></td>
+                                                <td> - </td>
+                                                <td> A project that tracks your daily expenses </td>
+                                                <td> <b>CodeIgniter 4</b> </td>
+                                                <td> <b>Html 5, CSS, Jquery, AdminLTE </b> (based on Bootstrap 5) </td>
+                                                <td> <b>MySQL</b> </td>
+                                                <td> <b>0.2.1</b> </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+
+    <div class="container">
+        <div class="col-xs-12">
+                <div class="text-center" style="padding-top: 10px; padding-bottom: 30px;">
+                    <a href="/" style="color: #ffffff;"> Back to the homepage </a>
+                </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>
